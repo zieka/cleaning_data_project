db3fb70821386732a559f0b02f5cdcb4ee89262c
Kyle Scully <scullykns@gmail.com>
2014-06-22 12:21:58 -0700

    data sets read in

---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+#
+# run_analysis.R
+#
+# Copyright (C) 2014 Kyle Scully
+#
+# Author(s)/Maintainer(s):
+# Kyle Scully
+#
+# This script does the following:
+#
+# 	* Merges the training and the test sets to create one data set.
+#
+#		* Extracts only the measurements on the mean and standard
+#			deviation for each measurement.
+#
+#		* Uses descriptive activity names to name the activities in the
+#			data set
+#
+#		* Appropriately labels the data set with descriptive variable
+#			names.
+#
+#		* Creates a second, independent tidy data set with the average of
+#			each variable for each activity and each subject.
+#
+
+# Read in features and activities
+features <- read.table('./features.txt',header=F);
+activity_labels <- read.table('./activity_labels.txt',header=F);
+
+# Read in training data
+x_train <- read.table('./UCI\ HAR\ Dataset/train/x_train.txt',header=F);
+y_train <- read.table('./train/y_train.txt',header=F);
+
+subject_train <- read.table('./train/subject_train.txt',header=F);
+
+# Read in test data
+x_test <- read.table('./test/x_test.txt',header=F);
+y_test <- read.table('./test/y_test.txt',header=F);
+subject_test <- read.table('./test/subject_test.txt',header=F);
+
+
+
+
+
+# Merge Data
+
+# Reshape Data
+
+# Save Data
